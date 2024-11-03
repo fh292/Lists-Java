2abcc160a981a5c3801343ca0c9f6dfc1a03ea7c
f h <falhamad2002@gmail.com>
2024-11-03 14:20:01 +0300

    task is done

--- src/ListsTask.java
+++ src/ListsTask.java
@@ -1,5 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
 public class ListsTask {
     public static void main(String[] args) {
 
+    //Create a List to hold your favorite songs/books/movies.
+        List <String> shows = new ArrayList<>();
+
+    //Add at least five songs/books/movies to your playlist.
+        shows.add("Friends");
+        shows.add("Prison Break");
+        shows.add("Modern Family");
+        shows.add("Brooklyn 99");
+        shows.add("Gilmore Girls");
+        System.out.println(shows);
+
+    //Remove the third song from the playlist.
+        shows.remove(2);
+        System.out.println(shows);
+
+    //Use a loop to display your playlist.
+        for (String show : shows) {
+            System.out.println(show);
+        }
     }
 }
